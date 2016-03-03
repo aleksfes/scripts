6647e8e6ec538f6f417153de1958e4252778b950
Alexander Feoktistov <Aleksandr_Feoktistov@epam.com>
2016-03-03 13:29:17 +0300

    install.sh: save email within .gitconfig after installing to use custom emails.

--- install.sh
+++ install.sh
@@ -55,9 +55,54 @@ echo ".vimrc installed."
 
 # git settings
 
-# Ставим свой gitconfig
-make_link $cfg_folder/gitconfig $home_folder/.gitconfig
+# Ставим свой gitconfig.
+# Так как на работе и дома я использую разные email,
+# то конфиг копируем с изменением нужных настроек.
+
+# Читаем из заданного git-конфига поле email,
+# чтобы не заставлять себя вводить этот email каждый раз.
+get_git_email() {
+	local git_config=$1
+	local email=$(grep -oP "email = \S+" $git_config | head -n 1 | grep -oP "\S+$")
+	echo $email
+}
+
+git_custom_config=$home_folder/.gitconfig
+git_reference_config=$cfg_folder/gitconfig
+
+# Но сначала достаём из местного конфига используемый email.
+git_custom_email=""
+if [ -f $git_old_config ]; then
+	git_custom_email="$(get_git_email $git_custom_config)"
+fi
+
+# Если кастомный email совпадает с референсным, то не спрашиваем ни о чём.
+git_reference_email="$(get_git_email $git_reference_config)"
+if [ "$git_reference_email" = "$git_custom_email" ]; then
+	git_custom_email=""
+fi
+
+# Чтобы использовать email из местного конфига я просто должен нажать Enter,
+# так будет проще запускать install.sh в будущем.
+if [ -n "$git_custom_email" ]; then
+	echo "Please, input anything to prevent using custom email (${git_custom_email}):"
+	read confirmation
+	if [ -n "$confirmation" ]; then
+		git_custom_email=""
+	fi
+fi
+
+# Копируем конфиг.
+rm -f $git_custom_config
+cp -f $git_reference_config $git_custom_config
 echo ".gitconfig installed."
+
+# Вставляем кастомный email на место.
+if [ -n "$git_custom_email" ]; then
+	sed -i -e "s/email\s\+=\s\+\S\+/email = ${git_custom_email}/g" $git_custom_config
+	echo "Custom email (${git_custom_email}) installed to .gitconfig."
+fi
+
 
 
 # finish
