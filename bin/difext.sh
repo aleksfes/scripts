87059ba278b869a1e30318a9384600bdaec36830
Alexander Feoktistov <alexander.feoktistov@corp.mail.ru>
2017-04-26 17:19:11 +0300

    difext: colorized diff (MacOS tested).

--- bin/difext.sh
+++ bin/difext.sh
@@ -18,6 +18,7 @@ show_help() {
 
 src=$1
 dst=$2
+show_diff=$3
 
 if [ -z "$src" -o -z "$dst" ]; then
   show_help
@@ -25,11 +26,17 @@ fi
 
 ### Actions
 
-cmd_output="less"
+diff_cmd="diff"
+show_result="less"
+
+has_colordiff="$(which colordiff)"
+if [ -n "$has_colordiff" ]; then
+  diff_cmd="colordiff"
+fi
 
 # определяем сравнение файлов
 if [ -f "$src" -a -f "$dst" ]; then
-  diff "$src" "$dst" | less
+  diff "$src" "$dst" | ${show_result}
 else
   # определяем сравнение каталогов
   if [ -d "$src" -a -d "$dst" ]; then
@@ -37,7 +44,12 @@ else
     # -r - рекурсивный обход директорий
     # -q - не выводит дифф между файлами, только сообщение
     # -y - вывод в две колонки (?)
-    diff -N -r -q -y --exclude="CVS" --exclude=".git" "$src" "$dst" | $cmd_output
+    # -c - вывод нескольких соседних строк для контекста
+    diff_options="-N -r -c"
+    if [ -z "$show_diff" ]; then
+      diff_options="${diff_options} -q"
+    fi
+    ${diff_cmd} ${diff_options} --exclude="CVS" --exclude=".git" "$src" "$dst" | ${show_result}
   # неправильные параметры
   else
     echo "ERROR: \"$src\" or \"$dst\" are not looking at file/folder!!!"
