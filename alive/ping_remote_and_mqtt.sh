9285e325727b32d9600061eb7f7bff9c0e9f16f0
Alexander Feoktistov <aleksfes@gmail.com>
2024-05-05 11:38:27 +0300

    ping_remote_and_mqtt.sh

--- alive/ping_remote_and_mqtt.sh
+++ alive/ping_remote_and_mqtt.sh
@@ -16,24 +16,24 @@ source ${SCRIPTS}/lib/tg.sh
 # --- PARAMS
 
 IP=$1
-# Человеко-понятное имя хоста, которое будет прописано в сообщении в ТГ.
-IP_ALIAS=$2
-BOT_TOKEN=$3
-CHAT_ID=$4
 # В этот файл будут писаться результаты некоторых пингов для отложенного разбора ситуации.
-PING_LOG_FILE=$5
-SKIP_TG_MESSAGE=$6
+PING_LOG_FILE=$2
+MQTT_TOPIC=$3
+MQTT_MESSAGE=$4
+MQTT_USER=$5
+MQTT_PASSWORD=$6
+MQTT_HOST=$7
+MQTT_PORT=$8
 
-if [[ -z "$IP" ]] || [[ -z "$IP_ALIAS" ]] || [[ -z "$BOT_TOKEN" ]] || [[ -z "$CHAT_ID" ]]; then
+if [[ -z "$IP" ]] || [[ -z "$MQTT_TOPIC" ]] || [[ -z "$MQTT_MESSAGE" ]] || [[ -z "$MQTT_USER" ]] \
+		|| [[ -z "$MQTT_PASSWORD" ]] || [[ -z "$MQTT_HOST" ]] || [[ -z "$MQTT_PORT" ]]; then
 	error_and_help "задайте параметры"
 fi
 if [[ ! -f $PING_LOG_FILE ]]; then
 	error_and_help "создайте лог-файл"
 fi
 
-# --- MAIN
-
-HOST_NAME="openvpn-server"
+# --- PING
 
 PING_IS_OK="PING_RESULT_OK"
 PING_IS_FAILED="PING_RESULT_FAILED"
@@ -61,26 +61,19 @@ done
 # dev-test purpose
 #PING_RESULT=1
 
-sendTgMessage() {
-	if [ -z "$SKIP_TG_MESSAGE" ]; then
-		sendMessage $1 $2 "$3"
-	fi
-}
+# --- PING RESULT HANDLING
 
 if [[ "$PING_RESULT" -eq "0" ]]; then
-	# Если сервер поднялся, то пишем об этом в чат.
 	if [[ ! "$LAST_PING_IS_OK" -eq "0" ]]; then
 		tsEcho "$PING_IS_OK $IP" >> $PING_LOG_FILE
-		MSG="Ура! Сервер $IP_ALIAS ($IP) вновь доступен с ${HOST_NAME}!"
-		sendTgMessage "$BOT_TOKEN" "$CHAT_ID" "$MSG"
 	fi
 else
 	# При фейлах пинга логируем всегда.
 	tsEcho "$PING_IS_FAILED $IP" >> $PING_LOG_FILE
 	# При первом фейле пишем в тг, остальные фейлы игнорируем до следующего успеха, чтобы не спамить.
 	if [[ "$LAST_PING_IS_OK" -eq "0" ]]; then
-		MSG="Сервер $IP_ALIAS ($IP) недоступен с ${HOST_NAME} :("
-		sendTgMessage "$BOT_TOKEN" "$CHAT_ID" "$MSG"
+		mqttx pub -t "$MQTT_TOPIC" -m "$MQTT_MESSAGE" -h "$MQTT_HOST" -p "$MQTT_PORT" \
+			-u "$MQTT_USER" -P "$MQTT_PASSWORD" > /dev/null
 	fi
 fi
 
